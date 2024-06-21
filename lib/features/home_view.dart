b5146b5c4d946b302885e172875f74e106ce622a
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2024-06-21 18:27:30 +0300

    feat: Add lottie and flutter_svg dependencies, and update asset configuration
    This commit adds the `lottie` and `flutter_svg` dependencies to the `pubspec.yaml` file. It also updates the asset configuration to include the necessary directories for colors, icons, images, and lotties. This change enables the integration of lottie and flutter_svg in the project.
    
    Recent user commits:
    - added localization feature and very good analysis
    - init app

--- lib/features/home_view.dart
+++ lib/features/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:horoscopai/products/asset/assets.gen.dart';
 import 'package:horoscopai/products/enums/device_locales.dart';
 import 'package:horoscopai/products/init/app_localizations.dart';
 import 'package:horoscopai/products/init/language/locale_keys.g.dart';
@@ -15,28 +16,28 @@ final class HomeView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Material App Bar'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(LocaleKeys.home_title).tr(),
-            const Text(LocaleKeys.home_description).tr(),
-            ElevatedButton(
-              onPressed: () {
-                context.locale == DeviceLocales.tr.locale
-                    ? AppLocalizations.changeLanguage(
-                        context: context,
-                        selectedLocale: DeviceLocales.en,
-                      )
-                    : AppLocalizations.changeLanguage(
-                        context: context,
-                        selectedLocale: DeviceLocales.tr,
-                      );
-              },
-              child: const Text(LocaleKeys.general_buttons_save).tr(),
-            ),
-          ],
-        ),
+      body: ListView(
+        children: <Widget>[
+          const Text(LocaleKeys.home_title).tr(),
+          const Text(LocaleKeys.home_description).tr(),
+          ElevatedButton(
+            onPressed: () {
+              context.locale == DeviceLocales.tr.locale
+                  ? AppLocalizations.changeLanguage(
+                      context: context,
+                      selectedLocale: DeviceLocales.en,
+                    )
+                  : AppLocalizations.changeLanguage(
+                      context: context,
+                      selectedLocale: DeviceLocales.tr,
+                    );
+            },
+            child: const Text(LocaleKeys.general_buttons_save).tr(),
+          ),
+          Assets.images.imgFlags.image(),
+          Assets.icons.icLove.svg(),
+          Assets.lotties.animZombie.lottie(),
+        ],
       ),
     );
   }
