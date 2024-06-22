a6137ea26d80714e0cac3508fc59046bb219034f
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2024-06-23 01:04:41 +0300

    dark theme completed

--- lib/products/asset/colors.gen.dart
+++ lib/products/asset/colors.gen.dart
@@ -13,12 +13,54 @@ import 'package:flutter/material.dart';
 class ColorName {
   ColorName._();
 
-  /// Color: #FF4081
-  static const Color colorAccent = Color(0xFFFF4081);
+  /// Color: #8A8AFE
+  static const Color blueViolet = Color(0xFF8A8AFE);
 
-  /// Color: #3F51B5
-  static const Color colorPrimary = Color(0xFF3F51B5);
+  /// Color: #0072FF
+  static const Color brandiesBlue = Color(0xFF0072FF);
 
-  /// Color: #303F9F
-  static const Color colorPrimaryDark = Color(0xFF303F9F);
+  /// Color: #0B022C
+  static const Color darkBackground = Color(0xFF0B022C);
+
+  /// Color: #6782FE
+  static const Color deepDenim = Color(0xFF6782FE);
+
+  /// Color: #FED694
+  static const Color eveningGlow = Color(0xFFFED694);
+
+  /// Color: #6BA7FE
+  static const Color fennelFlower = Color(0xFF6BA7FE);
+
+  /// Color: #71D8FE
+  static const Color heisenbergBlue = Color(0xFF71D8FE);
+
+  /// Color: #FC3DD3
+  static const Color matDazzleRose = Color(0xFFFC3DD3);
+
+  /// Color: #6EC4FE
+  static const Color mayaBlue = Color(0xFF6EC4FE);
+
+  /// Color: #73E9FE
+  static const Color mayasEgg = Color(0xFF73E9FE);
+
+  /// Color: #5F39FF
+  static const Color meteorShower = Color(0xFF5F39FF);
+
+  /// Color: #614FFE
+  static const Color primary = Color(0xFF614FFE);
+
+  /// Color: #FFE98D
+  static const Color quackQuack = Color(0xFFFFE98D);
+
+  /// Color: #72E4FE
+  static const Color robinsEgg = Color(0xFF72E4FE);
+
+  /// Color: #6256FE
+  static const Color shadyNeonBlue = Color(0xFF6256FE);
+
+  /// Color: #A140FF
+  static const Color sugarGrape = Color(0xFFA140FF);
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
 }
